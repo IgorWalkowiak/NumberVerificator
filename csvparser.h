5d26902c647df49c2e80aeed73176004a296bfee
Igor Walkowiak <IgorWalkowiak@protonmail.com>
2019-07-23 18:00:48 +0200

    clang format

--- csvparser.h
+++ csvparser.h
@@ -1,22 +1,19 @@
 #ifndef CSVPARSER_H
 #define CSVPARSER_H
 
-
 #include <fstream>
 #include <string>
 #include <vector>
 
-struct CoordinData{
+struct CoordinData {};
 
-};
+class CsvParser {
+  std::string source;
+  std::ifstream file;
 
-class CsvParser
-{
-    std::string     source;
-    std::ifstream   file;
 public:
-    CsvParser(std::string);
-    void parseFile(std::vector<std::pair<uint8_t,std::string>>&);
+  CsvParser(std::string);
+  void parseFile(std::vector<std::pair<uint8_t, std::string>> &);
 };
 
 #endif // CSVPARSER_H
--- neuronnetwork.h
+++ neuronnetwork.h
@@ -1,25 +1,22 @@
 #ifndef NEURONNETWORK_H
 #define NEURONNETWORK_H
-#include "inputdatadeliver.h"
 #include "Layers/firstlayer.h"
-#include "hiddennetwork.h"
-#include "Layers/outputlayer.h"
 #include "Layers/layeri.h"
+#include "Layers/outputlayer.h"
+#include "hiddennetwork.h"
+#include "inputdatadeliver.h"
 
-
-
-class NeuronNetwork
-{
-    InputDataDeliver inputDataDeliver;
-    FirstLayer firstLayer;
-    HiddenNetwork hiddenNetwork;
-    OutputLayer outputLayer;
+class NeuronNetwork {
+  InputDataDeliver inputDataDeliver;
+  FirstLayer firstLayer;
+  HiddenNetwork hiddenNetwork;
+  OutputLayer outputLayer;
 
 public:
-    NeuronNetwork(std::string csvFilePath, int numberOfHiddenLayers, int numberOfOutputs, int nodesInHiddenLayers);
-    void loadAndParseCsvDataFromFile();
-    void startLearn();
-
+  NeuronNetwork(std::string csvFilePath, int numberOfHiddenLayers,
+                int numberOfOutputs, int nodesInHiddenLayers);
+  void loadAndParseCsvDataFromFile();
+  void startLearn();
 };
 
 #endif // NEURONNETWORK_H
